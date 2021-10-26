b165d29cea8631276226c13383bdd92e0537b8c2
CrawLeyYou <kaanbenkansoy@gmail.com>
2021-10-26 05:27:41 +0300

    Upload Files

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSharp-HWID
+Simple Program just takes your HardwareID's, encoding with base64, and copies into your clipboard. (program separates hardware ids with "!")
+
+# License
+This project is licensed under [MIT](https://opensource.org/licenses/MIT) license.
---
+++ src/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text;
+using System.Management;
+
+namespace HWID
+{
+    class Class1
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var Processor = HWID("win32_processor", "processorID");
+            var GPU = HWID("win32_videocontroller", "driverversion");
+            var HDD = HWID("win32_diskdrive", "serialnumber");
+            var MotherBoard = HWID("win32_baseboard", "version");
+            var HardwareID = Processor + "!" + GPU + "!" + HDD + "!" + MotherBoard;
+            byte[] ASCII = ASCIIEncoding.ASCII.GetBytes(HardwareID);
+            string Base64 = Convert.ToBase64String(ASCII);
+            Clipboard.SetText(Base64);
+            MessageBox.Show("Your HWID is copied into your clipboard!");
+        }
+
+        private static string HWID(string Select, string ID)
+        {
+            ManagementClass Hardware = new ManagementClass(Select);
+            ManagementObjectCollection HardwareInfo = Hardware.GetInstances();
+            String HWIDa = null;
+            foreach (ManagementObject HWID in HardwareInfo)
+            {
+                HWIDa = HWID.Properties[ID].Value.ToString();
+                break;
+            }
+            return HWIDa;
+        }
+    }
+}
